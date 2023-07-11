4f4f168c76a68724e440485436ef45c97fd24122
Felipe Oliveira <flpoliveira42@gmail.com>
2023-07-11 17:39:13 -0300

    update readme

---
+++ app/(home)/layout.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+export const metadata = {
+  title: 'clone tabnews',
+  description: 'only for study',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/(home)/page.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <main>
+      <p>clone-tabnews</p>
+    </main>
+  );
+}
+
+export default Home;
