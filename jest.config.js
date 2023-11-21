3a2c2076d095ebbce0737447f2d7e07051e67168
Felipe Oliveira <flpoliveira42@gmail.com>
2023-11-21 18:18:18 -0300

    Update `.env` to `.env.development`. Update jest config with readable `.env.test`

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 const nextJest = require('next/jest')
 const createJestConfig = nextJest({ dir: "./" })
 
-module.exports = createJestConfig();
+module.exports = createJestConfig({
+  globalSetup: '<rootDir>/tests/setup.ts'
+});
