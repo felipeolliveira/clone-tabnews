6afc5c7b47b43e968c9894490a2fb59aa966b4d6
Felipe Oliveira <flpoliveira42@gmail.com>
2024-03-19 19:49:54 -0300

    finish  endpoint

--- src/app/api/v1/status/route.js
+++ src/app/api/v1/status/route.js
@@ -1,6 +1,29 @@
 import { query } from "infra/database";
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
-  return NextResponse.json({ status: 'healthy' }, { status: 200 })
+export async function GET() {
+  const databaseName = process.env.POSTGRES_DB
+
+  const maxConnections = await query('SHOW max_connections;')
+  const openedConnections = await query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [ databaseName ]
+  })
+  const version = await query('SHOW server_version;')
+
+  const maxConnectionsValue = maxConnections.rows[ 0 ].max_connections
+  const openedConnectionsValue = openedConnections.rows[ 0 ].count
+  const versionValue = version.rows[ 0 ].server_version
+
+  return NextResponse.json({
+    updated_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        status: 'healthy',
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: openedConnectionsValue,
+        version: versionValue
+      }
+    }
+  }, { status: 200 })
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test('GET to /api/v1/status should return 200', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status', { method: 'GET' })
+
   expect(response.status).toBe(200)
-})
+
+  const { dependencies, updated_at } = await response.json()
+
+  expect(updated_at).toBeDefined()
+  const parsedDate = new Date(updated_at).toISOString()
+  expect(parsedDate).toEqual(updated_at)
+  expect(dependencies.database.status).toBe('healthy')
+  expect(dependencies.database.version).toEqual("16.0")
+  expect(dependencies.database.max_connections).toBeGreaterThan(0)
+  expect(dependencies.database.opened_connections).toEqual(1)
+}) 
