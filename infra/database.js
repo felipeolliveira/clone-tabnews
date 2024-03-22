0b8a5926eececb83dd2e66255d898eac48f7ded6
Felipe Oliveira <flpoliveira42@gmail.com>
2024-03-22 19:09:58 -0300

    add  CA config

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(...queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'production'
+    ssl: getSSL()
   });
 
 
@@ -22,6 +22,16 @@ async function query(...queryObject) {
   } finally {
     await client?.end();
   }
+}
+
+function getSSL() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    }
+  }
+
+  return process.env.NODE_ENV !== 'development';
 }
 
 export {
