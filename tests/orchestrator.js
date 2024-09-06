21f90102b271ac88207108c36b6fcccdd37c90ad
Felipe Oliveira <flpoliveira42@gmail.com>
2024-09-06 17:13:53 -0300

    Improve orchestrator

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,14 +1,29 @@
 import retry from "async-retry"
 
+const WEB_SERVER_URL = 'http://localhost:3000'
+
 async function waitForWebServer() {
   const fetchFn = async () => {
-    const response = await fetch('http://localhost:3000/api/v1/status', { method: 'GET' })
-    await response.json()
+    try {
+      const response = await fetch(`${WEB_SERVER_URL}/api/v1/status`)
+
+      if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}`)
+      }
+    } catch (e) {
+      throw e
+    }
   }
 
   return retry(
     fetchFn,
-    { retries: 100, maxTimeout: 1000 }
+    {
+      retries: 100,
+      maxTimeout: 1000,
+      onRetry: (error, attempt) => {
+        console.log(`Attempt ${attempt} on fetch status page: ${error.message}`)
+      }
+    }
   )
 }
 
